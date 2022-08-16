9cff5e19c30d67fd510f7b38f7c14f4218b7f722
Setiawan Joko Prakoso <setyawanj65@gmail.com>
2022-08-16 08:07:18 +0700

    Add | Notes

--- objectGigi/Dokter.php
+++ objectGigi/Dokter.php
@@ -5,6 +5,7 @@ use Gigi\User;
 
 class Dokter extends User{ // extends => Inheritance || Pewarisan
     public String $no_reg;
+    // mendapatkan atribut username, email, password, dan login dari parent class (User)
 
     public function __construct(String $username, String $email, String $no_reg)
     {
